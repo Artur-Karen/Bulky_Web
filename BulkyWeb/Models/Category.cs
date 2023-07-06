a0a28aead9cd616a9f7cb0bfbf158e02b99ee966
Roman <rom080790@gmail.com>
2023-07-06 22:53:51 +0400

    ComplitedCrud

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(0, 100,ErrorMessage = "Values must be beetween 1 to 100 numbers")]
+        public int DisplayOrder { get; set; }
+    }
+}
